a520fba0605363c663cdea8f5f1447868030f4b5
marwenilo <marwenmagrii@gmail.com>
2019-11-08 16:19:36 +0100

    depand

--- src/route.js
+++ src/route.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home/Home';
-import Jobs from './pages/jobs/Jobs';
+
 import Header from './components/header';
 import Footer from './components/footer';
 import Login from './pages/login';
@@ -14,7 +14,6 @@ function Routee() {
         <Header />
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route path="/jobs" component={Jobs} />
             <Route exact path="/login" component={Login} />
             <Route  path="/inscription" component={Inscription} />
           </Switch>
